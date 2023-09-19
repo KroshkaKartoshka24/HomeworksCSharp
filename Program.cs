de15ac1ba010ff9877cc164c2e61db769d1e5389
Christina <nemovakg@gmail.com>
2023-09-19 19:01:52 +0300

    Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.

--- Program.cs
+++ Program.cs
@@ -1,21 +1,40 @@
-﻿try 
+﻿// try 
+// {
+//     Console.WriteLine("Введите первое число: ");
+//     int a = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Введите второе число: ");
+//     int b = Convert.ToInt32(Console.ReadLine());
+//     if (a > b)
+//     {
+//         Console.WriteLine($"max = {a}, min = {b}");
+//     }
+//     else if (a < b)
+//     {
+//         Console.WriteLine($"max = {b}, min = {a}");
+//     }
+//     else 
+//     {
+//         Console.WriteLine("Первое число равно второму");
+//     }
+// }
+// catch (Exception ex)
+// {
+//     Console.WriteLine("Введен неверный тип данных: " + ex.Message);
+// }
+
+using System;
+using System.Linq;
+
+try 
 {
+    double[] nums = new double[3];
     Console.WriteLine("Введите первое число: ");
-    int a = Convert.ToInt32(Console.ReadLine());
+    nums[0] = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine("Введите второе число: ");
-    int b = Convert.ToInt32(Console.ReadLine());
-    if (a > b)
-    {
-        Console.WriteLine($"max = {a}, min = {b}");
-    }
-    else if (a < b)
-    {
-        Console.WriteLine($"max = {b}, min = {a}");
-    }
-    else 
-    {
-        Console.WriteLine("Первое число равно второму");
-    }
+    nums[1] = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите третье число: ");
+    nums[2] = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Максимальное число = " + nums.Max());
 }
 catch (Exception ex)
 {
