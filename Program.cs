0f1d23859f9f813f989204c6613339611cb46d43
Christina <nemovakg@gmail.com>
2023-09-19 19:13:16 +0300

    Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)

--- Program.cs
+++ Program.cs
@@ -22,22 +22,40 @@
 //     Console.WriteLine("Введен неверный тип данных: " + ex.Message);
 // }
 
-using System;
-using System.Linq;
+// using System;
+// using System.Linq;
+
+// try 
+// {
+//     double[] nums = new double[3];
+//     Console.WriteLine("Введите первое число: ");
+//     nums[0] = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Введите второе число: ");
+//     nums[1] = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Введите третье число: ");
+//     nums[2] = Convert.ToInt32(Console.ReadLine());
+//     Console.WriteLine("Максимальное число = " + nums.Max());
+// }
+// catch (Exception ex)
+// {
+//     Console.WriteLine("Введен неверный тип данных: " + ex.Message);
+// }
 
 try 
 {
-    double[] nums = new double[3];
-    Console.WriteLine("Введите первое число: ");
-    nums[0] = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Введите второе число: ");
-    nums[1] = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Введите третье число: ");
-    nums[2] = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Максимальное число = " + nums.Max());
+    Console.WriteLine("Введите число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    if (num%2 == 0)
+    {
+        Console.WriteLine($"Число {num} является четным");
+    }
+    else 
+    {
+        Console.WriteLine($"Число {num} является нечетным");
+    }
+  
 }
 catch (Exception ex)
 {
     Console.WriteLine("Введен неверный тип данных: " + ex.Message);
 }
-
